365592e2ded03234bc14d14f0c7227b9c0420474
guzhongren <guzhongren@live.cn>
2020-05-03 21:06:05 +0800

    refactor: extract magic number

--- src/main/java/cn/xpbootcamp/refactor/Customer.java
+++ src/main/java/cn/xpbootcamp/refactor/Customer.java
@@ -5,6 +5,15 @@ import java.util.Vector;
 
 public class Customer implements CustomerInterface {
 
+    static final int NEW_RELEASE_PEER_DAY = 3;
+    static final double HISTORY_CONST_OF_OUT_2_DAYS = 1.5;
+    static final int HISTORY_BASE_DAYS = 2;
+    static final int HISTORY_CONST_IN_BASE_DAYS = 2;
+
+    static final double CAMPUS_CONST_IN_3_DAYS = 1.5;
+    static final int CAMPUS_BASE_DAYS = 3;
+    static final double CAMPUS_CONST_PEER_DAY = 1.5;
+
     private String name;
     private Vector<Rental> rentals = new Vector<>();
     private int frequentRenterPoints;
@@ -13,55 +22,79 @@ public class Customer implements CustomerInterface {
         this.name = name;
     }
 
-    public void addRental(Rental rental) {
-        rentals.addElement(rental);
+    String getName() {
+        return name;
     }
 
-    public String getName() {
-        return name;
+
+    public void addRental(Rental rental) {
+        rentals.addElement(rental);
     }
 
     public String statement() {
         double totalAmount = 0d;
         frequentRenterPoints = 0;
         Enumeration<Rental> rentals = this.rentals.elements();
-        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "：\n");
+        StringBuilder result = generateRentalHeader();
         while (rentals.hasMoreElements()) {
             Rental each = rentals.nextElement();
-            //show figures for this rental
-            //determine amounts for each line
-            double thisAmount = 0d;
-            switch (each.getMovie().getMovieType()) {
-                case HISTORY:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case CAMPUS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-            //add frequent renter points
-            frequentRenterPoints++;
-            if ((each.getMovie().getMovieType() == MovieType.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
-
-            //show figures for this rental
-            result.append("\t")
-                  .append(each.getMovie().getTitle())
-                  .append("\t")
-                  .append(thisAmount).append("\n");
-            totalAmount += thisAmount;
+            double thisAmount = getAmount(each);
+            getFrequentRenterPoints(each);
+            totalAmount = getSingleMovieInfo(totalAmount, result, each, thisAmount);
         }
-        //add footer lines
+        return generateRentalInfo(totalAmount, result);
+    }
+
+    private StringBuilder generateRentalHeader() {
+        return new StringBuilder("Rental Record for " + getName() + "：\n");
+    }
+
+    private double getAmount(Rental each) {
+        double thisAmount = 0d;
+        switch (each.getMovie().getMovieType()) {
+            case NEW_RELEASE:
+                thisAmount += each.getDaysRented() * NEW_RELEASE_PEER_DAY;
+                break;
+            case HISTORY:
+                thisAmount += HISTORY_CONST_IN_BASE_DAYS;
+                if (each.getDaysRented() > HISTORY_BASE_DAYS)
+                    thisAmount += (each.getDaysRented() - HISTORY_BASE_DAYS) * HISTORY_CONST_OF_OUT_2_DAYS;
+                break;
+            case CAMPUS:
+                thisAmount += CAMPUS_CONST_IN_3_DAYS;
+                int rentedDays = each.getDaysRented();
+                if (rentedDays > CAMPUS_BASE_DAYS)
+                    thisAmount += (rentedDays - CAMPUS_BASE_DAYS) * CAMPUS_CONST_PEER_DAY;
+                break;
+            default:
+                break;
+        }
+        return thisAmount;
+    }
+
+    private String generateRentalInfo(double totalAmount, StringBuilder result) {
         result.append("Amount owed is ").append(totalAmount).append("\n");
         result.append("You earned ").append(frequentRenterPoints).append(" frequent renter points");
         return result.toString();
+    }
+
+    private double getSingleMovieInfo(double totalAmount, StringBuilder result, Rental each, double thisAmount) {
+        result.append("\t")
+              .append(each.getMovie().getTitle())
+              .append("\t")
+              .append(thisAmount).append("\n");
+        totalAmount += thisAmount;
+        return totalAmount;
+    }
+
+    private void getFrequentRenterPoints(Rental each) {
+        frequentRenterPoints++;
+        if (isNewRelease(each.getMovie()) && each.getDaysRented() > 1)
+            frequentRenterPoints++;
+    }
+
+    private boolean isNewRelease(Movie movie) {
+        return movie.getMovieType() == MovieType.NEW_RELEASE;
     }
 
 }
